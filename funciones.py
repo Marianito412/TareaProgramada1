6b9b9b4e2a73e59eb82d6988d66a7bd8004d1f7a
Marianito412 <jmarianosoto@gmail.com>
2023-04-28 22:54:55 -0600

    Registrar anotaciones
    Se implementó la funcionalidad de registrar anotaciones en los expediented de los animales

---
+++ funciones.py
@@ -0,0 +1,12 @@
+
+def registrarAnotacion(pAnimal, pAnotacion):
+    """
+    Funcionalidad: Registra una nueva anotación en un animal específico
+    Entradas:
+    -pAnimal(list): El animal a modificar
+    -pAnotacion(str): La anotación a agregar
+    Salidas:
+    -pAnimal(list): El animal modificado
+    """
+    pAnimal[-1].append(pAnotacion)
+    return pAnimal
--- main.py
+++ main.py
@@ -7,6 +7,50 @@
 #print(a)
 #url = "https://es.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exsentences=8&exlimit=2&formatversion=2&origin=*&titles=Ailuropoda%20melanoleuca"
 
+import funciones
+def validarBin(pEntrada):
+    """
+    Funcionalidad: Valida una respuesta de si o no
+    Entradas:
+    -pEntrada(str): Texto conteniendo la opción
+    Salida:
+    return(bool): True si sí, False si no 
+    """
+    while True:
+        try:
+            if int(pEntrada) in [1,2]:
+                return int(pEntrada) == 1
+            else:
+                pEntrada = input("Entrada incorrecta se espera un 1 o 2, vuelva a intentar:\n1. Sí\n2. No\nOpción: ")
+        except ValueError:
+            pEntrada = input("Entrada incorrecta se espera un 1 o 2, vuelva a intentar:\n1. Sí\n2. No\nOpción: ")
+
+def ESRegistrarAnotaciones(animales):
+    """
+    Funcionalidad: Muestra interfaz para que el usuario pueda registrar anotaciones para un animal
+    Entradas:
+    -animales(list): Matriz de animales registrados
+    Salidas:
+    -animales(list): Matriz de animales registrados ahora con la nueva anotación
+    """
+    while True:
+        for indice, animal in enumerate(animales):
+            print(f"{indice+1}. {animal[0]}")
+        try:
+            eleccion = int(input("Escoja en qué animal desea registrar una nueva anotación: "))-1
+            anotacion = input(f"Ingrese la nueva anotación para {animales[eleccion][0]}:\n")
+            animales[eleccion] = funciones.registrarAnotacion(animales[eleccion], anotacion)
+            print("\nAnotaciones Registradas\n")
+            for indice, anotacion in enumerate(animales[eleccion][-1]): # Se asume que solo se necesita mostrar las anotaciones del animal relevante (basado en la elección del usuario)
+                print(f"{indice+1} {anotacion}")
+            if not validarBin(input("Desea registrar una nueva anotación?\n1. Sí\n2. No\nOpción: ")):
+                break
+        except ValueError:
+            print(f"Opción inválida ingrese un número del 1 al {len(animales)}")
+        except IndexError:
+            print(f"Opción inválida ingrese un número del 1 al {len(animales)}")
+    return animales
+
 def menu(pMatriz):
     """
     Funcionalidad: Muestra menú principal
@@ -30,7 +74,7 @@ def menu(pMatriz):
             try:
                 seguir=False
                 opcion = int(opcion)
-                opciones = [exit] #Registrar nuevas funcionalidades acá
+                opciones = [...,ESRegistrarAnotaciones,exit] #Registrar nuevas funcionalidades acá
                 #print(pMatriz)
                 pMatriz = opciones[opcion-1](pMatriz)
             except IndexError:
@@ -41,4 +85,4 @@ def menu(pMatriz):
                 print("La opción indicada no es un número")
                 opcion= input("Ingrese su opcion otra vez: ")
                 seguir=True
-menu([])
+menu([["Oso Polar", "Título", "URL", "Resumen", []], ["Jirafa Reticulada", "Título", "URL","Resumen", []]])
