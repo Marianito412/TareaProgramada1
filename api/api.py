1e27ab3fb5a22cbf6c76eabfa124ae7fac3cfaac
Marianito412 <jmarianosoto@gmail.com>
2023-04-29 16:12:06 -0600

    Limpiar info de wikipedia api

--- api/api.py
+++ api/api.py
@@ -5,8 +5,9 @@ import html
 wikipedia.set_lang("es")
 busqueda = "zorro gris patagónico"
 a = wikipedia.page("zorro gris patagónico")
-b=re.sub(r"\[\d*\]","",wikipedia.summary("zorro gris patagónico"))
-b.replace("\u200b","")
+b=wikipedia.summary("zorro gris patagónico")
+b=b.replace("\u200b", "")
+#b.replace("\u200b","")
 lista=["zorro gris patagónico",a.title, a.url,b]
 
 print(lista)
