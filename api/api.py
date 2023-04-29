2e90abdb8cf765998a0a67eb90ab32a137308e70
niparra21 <niparra@estudiantec.cr>
2023-04-28 23:01:45 -0600

    Prueba api

---
+++ api/api.py
@@ -0,0 +1,7 @@
+#import wikipedia
+#wikipedia.set_lang("es")
+#busqueda = "Oso panda"
+#a = wikipedia.summary(wikipedia.search(busqueda)[0])
+#
+#print(a)
+url = "https://es.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exsentences=8&exlimit=2&formatversion=2&origin=*&titles=Ailuropoda%20melanoleuca
