7e46895ab32503eb1d073cbba15b2122033102f5
Marianito412 <jmarianosoto@gmail.com>
2023-04-29 16:12:59 -0600

    sorry

--- api/api.py
+++ api/api.py
@@ -5,7 +5,7 @@ import html
 wikipedia.set_lang("es")
 busqueda = "zorro gris patagónico"
 a = wikipedia.page("zorro gris patagónico")
-b=wikipedia.summary("zorro gris patagónico")
+b=re.sub(r"\[\d*\]","",wikipedia.summary("zorro gris patagónico"))
 b=b.replace("\u200b", "")
 #b.replace("\u200b","")
 lista=["zorro gris patagónico",a.title, a.url,b]
